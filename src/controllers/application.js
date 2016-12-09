df6d64b2c33c5ddf457191a3d323e35da8b41566
egeneral <ephraimjohngeneral@Ephraims-MacBook-Pro.local>
2016-12-08 22:39:25 -0600

    Added find one, update one using object-mapper

--- src/controllers/application.js
+++ src/controllers/application.js
@@ -7,25 +7,51 @@ import ApplicationMap from '../models/application.map';
 export default({ config, db }) => {
   let api = Router();
 
-  // /v1/applications
-  api.get('/', (req, res) => {
-    res.json({message: "Ok Root."});
-  });
+  // CRUD operations
 
-  // /v1/applications/add
+  // /v1/applications/add - CREATE
   api.post('/add', (req, res) => {
     let newApplication = new Application(mapper(req.body, ApplicationMap));
 
     newApplication.save((err, application) => {
-      if (err) {
+      if (err)
         res.json(err);
-        //res.status(403).send({message: err.message});
-      }
-      else {
-        res.status(201).send(application);
-      }
 
+      res.status(201).send(application);
     });
+  });
+
+  // /v1/applications/ - READ ALL
+  api.get('/', (req, res) => {
+    Application.find({}, (err, applications) => {
+      if (err)
+        res.json(err);
+
+      res.status(201).send(applications);
+    });
+  });
+
+  // /v1/applications/:id - READ ONE
+  api.get('/:id', (req, res) => {
+    Application.find({_id:req.params.id}, (err, application) => {
+      if (err)
+        res.json(err);
+
+      res.status(201).send(application);
+    });
+  });
+
+  // /v1/applications/:id - UPDATE
+  api.put('/:id', (req, res) => {
+    Application.update(req.params.id,
+      {
+        $set: mapper(req.body, ApplicationMap)
+      }, (err, affected) => {
+        if (err)
+          res.json(err);
+
+        res.status(200).send({message: `${affected.nModified} rows affected`});
+      });
   });
 
   return api;
