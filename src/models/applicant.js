0af82b9a44d8f64b31f23175cc6adbe558f9bb90
egeneral <ephraimjohngeneral@Ephraims-MacBook-Pro.local>
2016-12-09 21:15:15 -0600

    Added properties to Applicant model and length restriction to Application

--- src/models/applicant.js
+++ src/models/applicant.js
@@ -1,7 +1,58 @@
 import mongoose from 'mongoose';
 
 let applicantSchema = mongoose.Schema({
-  
+  email: {
+    type: String,
+    required: true,
+    length: 50
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  givenName: {
+    type: String,
+    length: 25,
+    required: true
+  },
+  lastName: {
+    type: String,
+    length: 25,
+    required: true
+  },
+  middleName: {
+    type: String,
+    length: 25
+  },
+  birthday: {
+    type: Date,
+    required: true
+  },
+  isConfirmed: {
+    type: Boolean,
+    default: false
+  },
+  lastLogin: {
+    type: Date
+  },
+  isAccountLocked: {
+    type: Boolean
+  },
+  accountStatus: [
+    {
+      statusName: {
+        type: String,
+        required: true
+      },
+      statusDate: {
+        type: Date,
+        default: Date.now
+      },
+      updatedBy: {
+        type: String
+      }
+    }
+  ]
 });
 
 module.exports = mongoose.model('Applicant', applicantSchema);
